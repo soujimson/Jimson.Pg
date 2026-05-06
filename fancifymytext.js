686b60fbc965d7ac42a6ac548e6761886c945586
jimson2 <jimson2@gmail.com>
2026-05-05 19:44:17 -0700

    Assignment 5 Completed

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,5 +1,22 @@
 function alertButton() {
   document.getElementById("textarea1").style.fontSize= "24pt";
-  //alert("Hello, world!");
 }
 
+function changeStyle() {
+  alert("Fancify!");
+  document.getElementById("textarea1").style.fontWeight="bold";
+  document.getElementById("textarea1").style.color="blue";
+  document.getElementById("textarea1").style.textDecoration="underline";
+}
+
+function fixStyle() {
+  document.getElementById("textarea1").style.fontWeight="normal"
+}
+
+function moo() {
+  var upper = document.getElementById("textarea1").value.toUpperCase();
+  document.getElementById("textarea1").value = upper;
+  var parts = upper.split(".");
+  upper = parts.join("-Moo. ");
+  document.getElementById("textarea1").value = upper;
+}
